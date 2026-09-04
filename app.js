b2967cc5ec1b95cc8dce95b79a7305ed1daecf34
Lukratoo <Lukratoo4@gmail.com>
2026-09-04 14:58:44 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,4 @@
+playlist = [
+    {title: "pink pony club", artist: "chappell Roan", cover: "gato.jpg", src: "pink.wav"},
+    {title: "Jigsaw Falling into place", artist: "Radiohead", cover: "gato.jpg", src: "jigsaw.mp3"},
+]
